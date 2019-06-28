f47ecce48d51c13af9bcfd15fc43b3a356da9096
Kas Thomas <47995649+kasthomas2@users.noreply.github.com>
2019-06-28 12:50:28 -0700

    update HOME_URL

--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -2,7 +2,7 @@
 const API_ENDPOINT = "https://api.veritone.com/v3/graphql";
 const AUTH_BASE  = "https://api.veritone.com/v1/admin/oauth/authorize?scope=all&response_type=token&client_id=";
 const CLIENT_ID = "caf06532-6787-45f3-a2e4-8ff02a55012f";
-const HOME_URL = "https://vtn-integration-demo.netlify.com";
+const HOME_URL = "https://veritone-sample-app.netlify.com";
 const TEXT_VALIDATION_ERROR_MSG = "That doesn't look right. Try again.";
 const DAYS_TO_STORE_TOKEN = 1;
 let TDO_ID = null;
