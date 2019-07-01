91a72bcaaa91b790d79114848241544f12c09bd7
Kas Thomas <47995649+kasthomas2@users.noreply.github.com>
2019-07-01 12:17:24 -0700

    License info and copyright

--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -1,3 +1,23 @@
+/* 'K2' Tutorial App v1.0.0
+
+ Copyright 2019, Veritone, Inc.
+ 
+ For more information about this app, go to: https://docs.veritone.com/#/developer
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+*/
+
 // GLOBALS
 const API_ENDPOINT = "https://api.veritone.com/v3/graphql";
 const AUTH_BASE  = "https://api.veritone.com/v1/admin/oauth/authorize?scope=all&response_type=token&client_id=";
